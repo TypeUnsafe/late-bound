10729ef73e5899f18ab76650a728a7cf7c25c41f
Julien Ponge <julien.ponge@insa-lyon.fr>
2014-07-25 15:38:04 +0200

    Javadocs

--- src/main/java/org/typeunsafe/latebound/LateBound.java
+++ src/main/java/org/typeunsafe/latebound/LateBound.java
@@ -22,9 +22,29 @@
 
 package org.typeunsafe.latebound;
 
+/**
+ * A factory to ease late-binding at runtime of environment-specific implementation classes.
+ *
+ * This class is not thread-safe. The target class selection is done just once over the lifetime of a {@code LateBound}
+ * instance.
+ *
+ * The {@code T} parametric type is not enforced at runtime. That is the beauty of generic types not being available
+ * for reflective inspection.
+ *
+ * @param <T> The type of an interface or base class for implementations.
+ */
 public class LateBound<T> {
 
+  /**
+   * A functional interface to select a target implementation class at runtime.
+   */
   public static interface Evaluator {
+
+    /**
+     * Perform the target class selection.
+     *
+     * @return the selected qualified class name.
+     */
     String apply();
   }
 
@@ -33,15 +53,32 @@ public class LateBound<T> {
 
   private Class<T> targetClass;
 
+  /**
+   * Creates a new {@code LateBound} instance.
+   *
+   * @param evaluator   an evaluator.
+   * @param classLoader a classlaoder.
+   */
   public LateBound(Evaluator evaluator, ClassLoader classLoader) {
     this.evaluator = evaluator;
     this.classLoader = classLoader;
   }
 
+  /**
+   * Creates a new {@code LateBound} instance with the classloader used to load the {@code LateBound} class itself.
+   *
+   * @param evaluator an evaluator.
+   */
   public LateBound(Evaluator evaluator) {
     this(evaluator, LateBound.class.getClassLoader());
   }
 
+  /**
+   * Target class accessor, which may trigger the one-time target class selection.
+   *
+   * @return a new instance.
+   * @throws ClassNotFoundException if the target class cannot be loaded.
+   */
   @SuppressWarnings("unchecked")
   public Class<T> targetClass() throws ClassNotFoundException {
     if (targetClass == null) {
@@ -50,6 +87,14 @@ public class LateBound<T> {
     return targetClass;
   }
 
+  /**
+   * Creates a new instance, which may trigger the one-time target class selection.
+   *
+   * The target class must have a no-arguments public constructor.
+   *
+   * @return a new instance.
+   * @throws ReflectiveOperationException in case the class cannot be loaded or instantiated.
+   */
   public T newInstance() throws ReflectiveOperationException {
     if (targetClass == null) {
       targetClass();
